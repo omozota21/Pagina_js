9a6f18f4c6813e323888ef1ce3f66c00f3710e25
omozota21 <148052067+omozota21@users.noreply.github.com>
2024-03-03 18:38:22 +0100

    .

--- js/index.js
+++ js/index.js
@@ -45,13 +45,44 @@ function incrementCounter() {
 
 
 
+/* Tipos de Variables */ 
+var ciudad = "Utebo";
+let edad = 18;
+const nombre = "Oscar";
 
 
 
+/* Lista de Tareas */
+const addNewTask = event => {
+    event.preventDefault();
+    const { value } = event.target.taskText;
+    if(!value) return;
+    const task = document.createElement('div');
+    task.classList.add('task', 'roundBorder');
+    task.addEventListener('click', changeTaskState)
+    task.textContent = value;
+    tasksContainer.prepend(task);
+    event.target.reset();
+};
 
+const changeTaskState = event => {
+    event.target.classList.toggle('done');
+};
 
+const order = () => {
+    const done = [];
+    const toDo = [];
+    tasksContainer.childNodes.forEach( el => {
+        el.classList.contains('done') ? done.push(el) : toDo.push(el)
+    })
+    return [...toDo, ...done];
+}
 
+const renderOrderedTasks = () => {
+    order().forEach(el => tasksContainer.appendChild(el))
+}
 
+setDate();
 
 
 
